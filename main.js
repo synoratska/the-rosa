061b8ff4be4a2910dcf0136ac6a3e87104a30c82
synoratska <m.synoratska@gmail.com>
2022-05-09 16:21:50 +0300

    final changes

--- main.js
+++ main.js
@@ -1,45 +1,43 @@
 // Select element function
-const selectElement = function(element) {
-    return document.querySelector(element);
-}
+const selectElement = function (element) {
+  return document.querySelector(element);
+};
 
-const menuToggler = selectElement('.menu-toggle');
-const body = selectElement('body');
+const menuToggler = selectElement(".menu-toggle");
+const body = selectElement("body");
 
-menuToggler.addEventListener('click', () => {
-    body.classList.toggle('open');
-})
-
+menuToggler.addEventListener("click", () => {
+  body.classList.toggle("open");
+});
 
 // Scroll reveal
 
 window.sr = ScrollReveal();
 
-sr.reveal('.animate-left', {
-    origin: 'left',
-    duration: 1000,
-    distance: '25rem',
-    delay: 300
-})
+sr.reveal(".animate-left", {
+  origin: "left",
+  duration: 1000,
+  distance: "25rem",
+  delay: 300,
+});
 
+sr.reveal(".animate-right", {
+  origin: "right",
+  duration: 1000,
+  distance: "25rem",
+  delay: 600,
+});
 
-sr.reveal('.animate-right', {
-    origin: 'right',
-    duration: 1000,
-    distance: '25rem',
-    delay: 600
-})
+sr.reveal(".animate-top", {
+  origin: "top",
+  duration: 1000,
+  distance: "25rem",
+  delay: 600,
+});
 
-sr.reveal('.animate-top', {
-    origin: 'top',
-    duration: 1000,
-    distance: '25rem',
-    delay: 600
-})
-
-sr.reveal('.animate-bottom', {
-    origin: 'bottom',
-    duration: 1000,
-    distance: '25rem',
-    delay: 600
-})
+sr.reveal(".animate-bottom", {
+  origin: "bottom",
+  duration: 1000,
+  distance: "25rem",
+  delay: 600,
+});
